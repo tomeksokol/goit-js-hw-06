0285e0fa4c9db793bd2408f0253ebe7445de451e
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-21 22:38:38 +0200

    3

--- js/task-01.js
+++ js/task-01.js
@@ -5,8 +5,18 @@ console.log(`Number of categories: ${numberOfCategories.length}`);
 
 //Animals
 
-const animals = document.querySelector('.item > h2');
+const animals = document.querySelector('h2');
 console.log(`Category: ${animals.textContent}`);
 
 const numberOfAnimals = document.querySelectorAll('.item li');
 console.log(`Elements: ${numberOfAnimals.length}`);
+//Categories
+
+const firstCategory = document.querySelectorAll('.item > h2');
+console.log(`Category: ${firstCategory[0].textContent}`);
+
+const secondCategory = document.querySelectorAll('.item > h2');
+console.log(`Category: ${secondCategory[1].textContent}`);
+
+const thirdCategory = document.querySelectorAll('.item > h2');
+console.log(`Category: ${thirdCategory[2].textContent}`);
