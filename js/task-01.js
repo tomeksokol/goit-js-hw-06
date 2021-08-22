cb5244e2e617b13de87a7c00ddcc797306150ca6
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-22 10:54:07 +0200

    task_2

--- js/task-01.js
+++ js/task-01.js
@@ -5,23 +5,23 @@ console.log(`Number of categories: ${numberOfCategories.length}`);
 
 //First Category
 
-const firstCategory = document.querySelectorAll('.item > h2');
-console.log(`Category: ${firstCategory[0].textContent}`);
+const firstCategoryName = document.querySelectorAll('.item > h2');
+console.log(`Category: ${firstCategoryName[0].textContent}`);
 
 const elementNumberCat1 = document.querySelectorAll('.item > ul');
 console.log(`Elements: ${elementNumberCat1[0].children.length}`);
 
 //Second Category
 
-const secondCategory = document.querySelectorAll('.item > h2');
-console.log(`Category: ${secondCategory[1].textContent}`);
+const secondCategoryName = document.querySelectorAll('.item > h2');
+console.log(`Category: ${secondCategoryName[1].textContent}`);
 
 const elementNumberCat2 = document.querySelectorAll('.item > ul');
 console.log(`Elements: ${elementNumberCat2[1].children.length}`);
 
 //Third Category
-const thirdCategory = document.querySelectorAll('.item > h2');
-console.log(`Category: ${thirdCategory[2].textContent}`);
+const thirdCategoryName = document.querySelectorAll('.item > h2');
+console.log(`Category: ${thirdCategoryName[2].textContent}`);
 
 const elementNumberCat3 = document.querySelectorAll('.item > ul');
 console.log(`Elements: ${elementNumberCat3[2].children.length}`);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,54 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+console.log(list);
+
+const firstIngredient = document.createElement('li');
+firstIngredient.textContent = ingredients[0];
+firstIngredient.classList.add('item');
+console.log(firstIngredient);
+
+const secondIngredient = document.createElement('li');
+secondIngredient.textContent = ingredients[1];
+secondIngredient.classList.add('item');
+console.log(secondIngredient);
+
+const thirdIngredient = document.createElement('li');
+thirdIngredient.textContent = ingredients[2];
+thirdIngredient.classList.add('item');
+console.log(thirdIngredient);
+
+const fourthIngredient = document.createElement('li');
+fourthIngredient.textContent = ingredients[3];
+fourthIngredient.classList.add('item');
+console.log(fourthIngredient);
+
+const fifthIngredient = document.createElement('li');
+fifthIngredient.textContent = ingredients[4];
+fifthIngredient.classList.add('item');
+console.log(fifthIngredient);
+
+const sixthIngredient = document.createElement('li');
+sixthIngredient.textContent = ingredients[5];
+sixthIngredient.classList.add('item');
+console.log(sixthIngredient);
+
+list.append(
+  firstIngredient,
+  secondIngredient,
+  thirdIngredient,
+  fourthIngredient,
+  fifthIngredient,
+  sixthIngredient
+);
+
+// the second way with innerHTML
+
+// const markup = ingredients
+// .map((ingredient) => `<li class="item"> ${ingredient}</li>`)
+// .join("");
+// console.log(markup);
+
+// list.innerHTML = markup;
