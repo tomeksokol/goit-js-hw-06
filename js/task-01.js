93f0352b0f596b18c7eb629b2bb0753afaed3dec
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-22 09:28:31 +0200

    task_1_finished

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,27 @@
 //number of categories
 
-const numberOfCategories = document.querySelectorAll('.item');
+const numberOfCategories = document.querySelectorAll('#categories .item > h2');
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
-//Animals
-
-const animals = document.querySelector('h2');
-console.log(`Category: ${animals.textContent}`);
-
-const numberOfAnimals = document.querySelectorAll('.item li');
-console.log(`Elements: ${numberOfAnimals.length}`);
-//Categories
+//First Category
 
 const firstCategory = document.querySelectorAll('.item > h2');
 console.log(`Category: ${firstCategory[0].textContent}`);
 
+const elementNumberCat1 = document.querySelectorAll('.item > ul');
+console.log(`Elements: ${elementNumberCat1[0].children.length}`);
+
+//Second Category
+
 const secondCategory = document.querySelectorAll('.item > h2');
 console.log(`Category: ${secondCategory[1].textContent}`);
 
+const elementNumberCat2 = document.querySelectorAll('.item > ul');
+console.log(`Elements: ${elementNumberCat2[1].children.length}`);
+
+//Third Category
 const thirdCategory = document.querySelectorAll('.item > h2');
 console.log(`Category: ${thirdCategory[2].textContent}`);
+
+const elementNumberCat3 = document.querySelectorAll('.item > ul');
+console.log(`Elements: ${elementNumberCat3[2].children.length}`);
