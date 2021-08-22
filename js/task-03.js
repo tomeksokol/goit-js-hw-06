8aa89f5a7bd9e43ccc8ce3d2e246f739ff950a74
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-22 16:45:16 +0200

    taks_3_finished

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,36 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList);
+
+const listOfImages = images
+  .map(
+    image =>
+      `<li class="item"> <img class="photo" src="${image.url}" alt="${image.alt}"></li>`
+  )
+  .join('');
+console.log(listOfImages);
+
+galleryList.insertAdjacentHTML('beforeend', listOfImages);
+
+const galleryStyle = document.querySelector('.gallery');
+galleryStyle.style.listStyle = 'none';
+galleryStyle.style.display = 'flex';
+galleryStyle.style.flexDirection = 'column';
+galleryStyle.style.justifyContent = 'center';
+galleryStyle.style.alignItems = 'center';
+galleryStyle.style.marginBottom = '10px';
+
+const photoStyle = document.querySelectorAll('.gallery .item .photo');
+const firstPhoto = photoStyle[0];
+firstPhoto.style.marginBottom = '20px';
+firstPhoto.style.width = 'calc(95vw - 30px)';
+
+const secondPhoto = photoStyle[1];
+secondPhoto.style.width = 'calc(95vw - 30px)';
+secondPhoto.style.marginBottom = '20px';
+
+const thirdPhoto = photoStyle[2];
+thirdPhoto.style.width = 'calc(95vw - 30px)';
