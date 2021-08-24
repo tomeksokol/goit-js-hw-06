2bc9b9bd50a6f9ac629dd30b1c776e3c846878d1
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-24 14:25:41 +0200

    task_10_partially

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberOfBoxes = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+const num = event => {
+  let amountOfBoxes = event.currentTarget.value;
+  console.log(amountOfBoxes);
+};
+
+const box = event => {
+  let div = document.createElement('div');
+  div.classList.add('newDiv');
+  div.style.width = '30px';
+  div.style.height = '30px';
+  div.style.backgroundColor = getRandomHexColor();
+  boxes.append(div);
+  console.log(div);
+};
+
+function createBoxes(amount) {}
+
+const removeBox = event => {
+  let removeDiv = document.querySelectorAll('.newDiv');
+  console.log(removeDiv);
+  removeDiv[0].remove();
+};
+
+numberOfBoxes.addEventListener('input', num);
+createBtn.addEventListener('click', box);
+destroyBtn.addEventListener('click', removeBox);
