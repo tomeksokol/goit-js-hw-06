cd20c66d2b6f9a6dc5ccf9fedd71e753ebcc2066
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-24 12:26:30 +0200

    Task_9_finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('button.change-color');
+const colorNumber = document.querySelector('span.color');
+
+const changeColor = event => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorNumber.textContent = getRandomHexColor();
+  console.log(colorNumber.textContent);
+};
+
+changeColorBtn.addEventListener('click', changeColor);
